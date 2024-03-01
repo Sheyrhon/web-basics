b73358d85f74e2f9fa375c05a1684571d4a5a13e
Sheyrhon <chisheyrhon96@gmail.com>
2024-03-01 22:46:33 +0100

    started react app

---
+++ acordian/src/components/Accordion/data.js
@@ -0,0 +1,23 @@
+const data=[
+    {
+        id:'1',
+        question:"What are accordion components?",
+        answer:"Accordion components are user interface elements used for organizing.."
+    },
+    {
+        id:'2',
+        question:"What are accordion components?",
+        answer:"Accordion components are user interface elements used for organizing.."
+    },
+    {
+        id:'3',
+        question:"What are accordion components?",
+        answer:"Accordion components are user interface elements used for organizing.."
+    },
+    {
+        id:'4',
+        question:"What are accordion components?",
+        answer:"Accordion components are user interface elements used for organizing.."
+    }
+]
+export default data;
